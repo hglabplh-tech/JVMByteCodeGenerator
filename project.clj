f5d50706ec059f6c9f0b12bfdbe31485facc4add
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2024-01-16 00:48:28 +0100

    init

---
+++ project.clj
@@ -0,0 +1,29 @@
+(defproject hgp.genbytec/JVMByteCodeGenerator "1.0.0-SNAPSHOT" ; version "1.0.0-SNAPSHOT
+  :description "A 370 / 390 Assembler"
+  :min-lein-version "2.0.0"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :main hgp.ass370/ass-main
+  :dependencies
+  [[org.clojure/clojure "1.11.1"]
+   [org.ow2.asm/asm "9.6"]
+   [org.ow2.asm/asm-commons "9.6"]
+   [org.ow2.asm/asm-util "9.6"]
+   [org.ow2.asm/asm-tree "9.6"]
+   [org.ow2.asm/asm-analysis "9.6"]
+   [org.clojure/core.async "1.6.681"]
+   [de.active-group/active-clojure "0.42.2"]
+   [clojure-interop/apache-commons-io "1.0.0"]]
+  :source-paths ["src"]
+  :java-source-paths ["src/main/java"]                      ; Java source is stored separately.
+  :test-paths ["test/clj"]
+  :resource-paths ["resource"]
+  :dev {:resource-paths "test/resource"}
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled"
+                                    "resources/public/cljs-out" :target-path]
+  :aot :all
+  :jar-exclusions [#"(?:^|/).svn/.idea"]
+  :jar-inclusions [#"(?:^|/)\.ebextensions"]
+
+  )

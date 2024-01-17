ea555733d687bdd8554ca7c41fc2eb70cde8ee90
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2024-01-18 00:38:44 +0100

    first step to really generate code in clojure

---
+++ src/hgp/genbytec/generator/generators/code_gen.clj
@@ -0,0 +1,29 @@
+(ns hgp.genbytec.generator.generators.code-gen
+  (:import (genbytecj.generator.model.startpoint GenerationController)
+           (genbytecj.generator.model.utils Randomizer RandomSupplier)
+           ))
+
+(defn new-code-gen[]
+  (let [gen-controler (GenerationController.)
+        rand-code-gen (.initRandCodeGen gen-controler)
+        field-var-generator (.getFieldVarGenerator rand-code-gen)
+        method-generator (.getMethodGenerator rand-code-gen)
+        math-generator (.getMathGenerator rand-code-gen)
+        snippet-generator (.getSnippetGenerator rand-code-gen)
+        type-cast-generator (.getTypeCastGenerator rand-code-gen)
+        control-flow-generator (.getControlFlowGenerator rand-code-gen)
+        array-access-generator (.getArrayAccessGenerator rand-code-gen)]
+    {:field-var-gen field-var-generator :method-gen method-generator
+     :math-gen math-generator :snippet-gen snippet-generator
+     :type-cast-gen type-cast-generator :control-flow-gen control-flow-generator
+     :array-access-gen array-access-generator}
+
+
+    )
+
+
+  )
+
+
+
+

deb32a2d2b288123ddf1492ec7ec303489038bf3
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2024-02-18 21:46:53 +0100

    commit with the code we use from the jasmin assembler

---
+++ src/main/java/jasmin/utils/generate/jasmin/relative_num_token.java
@@ -0,0 +1,16 @@
+
+package jasmin.utils.generate.jasmin;
+import jasmin.utils.java_cup.*;
+
+/** This subclass of token represents numbers beginning with '+' or '-'
+ *
+ * @see java_cup.runtime.str_token
+ * @version last updated: 26/10/05
+ * @author  Jon Meyer
+ */
+
+class relative_num_token extends jasmin.utils.java_cup.runtime.int_token {
+    public relative_num_token(int term_num, int v) {
+        super(term_num, v);
+    }
+}
